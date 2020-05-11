8615373ae697ffe89b9fa6f5f75d572fbdfa3eba
Duyen Rice <duyenqt98@gmail.com>
2020-05-11 18:37:20 +0700

    Create Account Model

---
+++ api/models/account.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const accountSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean
+    },
+    created_at: {
+        type: Date
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    updated_at: {
+        type: Date
+    },
+    updated_by: {
+        type: mongoose.Schema.Types.ObjectId
+    }
+});
+
+module.exports = mongoose.model('Account', accountSchema);
