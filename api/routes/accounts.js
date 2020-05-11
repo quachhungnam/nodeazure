492f9560345d4486657136866bfe7ba89b5b4ce5
Duyen Rice <duyenqt98@gmail.com>
2020-05-11 18:43:07 +0700

    Create routes for /accounts

---
+++ api/routes/accounts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+// const multer = require('multer');
+
+const router = express.Router();
+
+const checkAuth = require("../middlewares/check-auth");
+const AccountController = require("../controllers/accounts");
+
+router.get("/", checkAuth, AccountController.accounts_get_all);
+
+router.get("/:accountId", checkAuth, AccountController.accounts_get_account);
+
+router.post("/signup", AccountController.account_signup);
+
+router.post("/login", AccountController.account_login);
+
+router.patch(
+  "/:accountId",
+  checkAuth,
+  AccountController.accounts_update_account
+);
+
+router.delete(
+  "/:accountId",
+  checkAuth,
+  AccountController.accounts_delete_account
+);
+
+module.exports = router;
